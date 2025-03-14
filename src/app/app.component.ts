00e986b68e54de0fa588de82f3c8fb58a6d6e771
Angel Gaitan <angel.gaitan.ev@gmail.com>
2025-03-14 20:01:07 -0300

    added all features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderNavComponent } from './shared/components/header-nav/header-nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
