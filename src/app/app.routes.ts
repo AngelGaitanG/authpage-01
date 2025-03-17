fab20db6485a59fa886dc60af01adaaf2bf932d6
Angel Gaitan <angel.gaitan.ev@gmail.com>
2025-03-17 17:34:24 -0300

    Complete home, login and register views with responsive design implementation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,5 +16,10 @@ export const APP_ROUTES: Routes = [
         path: 'home',
         loadChildren: () => import('./features/home/home.routes')
             .then(m => m.HOME_ROUTES)
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
     }
 ]
